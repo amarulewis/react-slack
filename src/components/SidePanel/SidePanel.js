62cba620a512724d3c5086d38e51104ae7e0ed08
amaru <73050918+amarulewis@users.noreply.github.com>
2021-08-21 02:07:56 -0400

    Integrate Redux, chat console
    Redux for global state
    Update App.js with component scaffold
    Added UserPanel dropdown

---
+++ src/components/SidePanel/SidePanel.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import UserPanel from './UserPanel';
+import {Menu} from 'semantic-ui-react';
+
+
+class SidePanel extends React.Component {
+    render() {
+        return (
+            <Menu 
+                size="large"
+                inverted
+                fixed="left"
+                vertical
+                style={{background: '#4c3c4c', fontsize: '1.2rem'}}
+            >
+                <UserPanel />
+            </Menu>
+        )
+    }
+}
+
+export default SidePanel
